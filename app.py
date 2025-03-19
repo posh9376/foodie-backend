48876dfe04554ba1b91c145e8b52f047d3cd261e
Eric Richard <reric0802@gmail.com>
2025-03-19 05:30:08 +0300

    added port variables

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5376,debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get port from environment, default to 5000
+    app.run(debug=False, host='0.0.0.0', port=port)
